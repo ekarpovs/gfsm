d0149ed2c29757381b2352d2e801256a9817e8ae
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-06-21 07:24:18 +0300

    Convert user action decorator into wrapper

--- gfsm/action.py
+++ gfsm/action.py
@@ -1,12 +1,21 @@
 
-# Decorator for an action
+# Default user action's wrapper
 def fsm_action(implementation):
   def execute(context):
     __name__ = implementation.__name__
-    print("action", __name__)
+    print("default wrapper for action:", __name__)
+    # Separate user functions from a FSM
 
-    context = implementation(context)
+    # Get a relevant user data
+    key = "user_data"
+    data = context.get(key)
+
+    context = implementation(data)
     print("context", context.name)
+
+    # Store the new user data
+    context.put(key, data)
+
 
     return context
 
--- test/actions.py
+++ test/actions.py
@@ -1,51 +1,19 @@
 from gfsm import fsm_action
 
-@fsm_action
-def init_action(context):
-  # Get a relevant user data
-  key = "user_data"
-  data = context.get(key)
-  # Do initialiation
-
-  # Store the user data
-  context.put(key, data)
-
-  return context
+def init_action(data):
+  # Do thomething
+  return data
 
 
-@fsm_action
-def action1(context):
-  # Get a relevant user data
-  key = "user_data"
-  data = context.get(key)
-  # Do initialiation
-
-  # Store the user data
-  context.put(key, data)
-
-  return context
+def action1(data):
+  # Do thomething
+  return data
 
 
-@fsm_action
-def action2(context):
-  # Get a relevant user data
-  key = "user_data"
-  data = context.get(key)
-  # Run step
-  
-  # Store the user data
-  context.put(key, data)
+def action2(data):
+  # Do thomething
+  return data
 
-  return context
-
-@fsm_action
-def action3(context):
-  # Get a relevant user data
-  key = "user_data"
-  data = context.get(key)
-  # Run step
-  
-  # Store the user data
-  context.put(key, data)
-
-  return context
+def action3(data):
+  # Do thomething
+  return data
